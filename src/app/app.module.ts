4420a5a659c2e1bfc6f770d22cc40a966a9109d4
vivek padia <vivekpadia70@gmail.com>
2017-10-16 00:35:31 +0530

    setting up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,28 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { NgDatepickerModule } from 'ng2-datepicker';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { SchedulePage } from '../pages/schedule/schedule';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    SchedulePage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    NgDatepickerModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    SchedulePage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { SchedulePage } from '../../pages/schedule/schedule'
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,10 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  openPage(){
+    this.navCtrl.push(SchedulePage);
   }
 
 }
